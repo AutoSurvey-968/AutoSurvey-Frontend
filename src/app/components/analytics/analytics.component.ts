29c2236b5bb5ae10fd36f589d9a54314d1bbdfe7
kevinrose235 <kevinrose235@gmail.com>
2021-05-28 17:24:48 -0500

    scared of computer death

--- src/app/components/analytics/analytics.component.ts
+++ src/app/components/analytics/analytics.component.ts
@@ -7,6 +7,8 @@ import {
   MatSnackBarVerticalPosition,
 } from '@angular/material/snack-bar';
 
+import { AnalyticsService } from '../../services/analytics/analytics.service';
+
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
@@ -18,22 +20,20 @@ export class AnalyticsComponent implements OnInit {
     start: new FormControl(),
     end: new FormControl()
   });
-  constructor(private _snackBar: MatSnackBar) { }
+  constructor(private _snackBar: MatSnackBar, private analyticsService: AnalyticsService) { }
   ngOnInit(): void {
 
   }
+  pipe = new DatePipe('en-US');
   getData(){
     if (this.active==1 && this.range.get('start')!=null &&this.range.get('start')?.value!=null ){
-      alert(this.range.get('start')?.value );
+      this.analyticsService.getDataWeek(this.pipe.transform(this.range.get('start')?.value, 'yyyy-MM-dd')||"");
     }
     else if (this.active==2 && this.range.get('start')!=null &&this.range.get('start')?.value!=null ){
-     // alert({{this.range.get('start')?.value | json}} );
-    }
+      this.analyticsService.getDataWeekBatch(this.pipe.transform(this.range.get('start')?.value, 'yyyy-MM-dd')||"", "");    }
     else{
-     
       this._snackBar.open("Form isn't fully filled out", "Okay");
-      setTimeout(this._snackBar.dismiss.bind(this._snackBar), 1500);
-      
+      setTimeout(this._snackBar.dismiss.bind(this._snackBar), 2000);
     }
     
   }
--- src/app/services/analytics/analytics.service.ts
+++ src/app/services/analytics/analytics.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyticsService {
+  endpoint: string = environment.apiUrl+'/';
+  private httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }), withCredentials:true};
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getDataWeek(startDate: string){
+    let body={
+      "startDate":startDate
+    }
+    return this.http.put(this.endpoint, body,this.httpOptions).pipe(
+      map(response => response as string)
+    );
+  }
+  getDataWeekBatch(startDate: string, batchId: string){
+
+  }
 }
