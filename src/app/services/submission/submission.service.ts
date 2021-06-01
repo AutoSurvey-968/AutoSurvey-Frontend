2d8dcdf0ac2bcbcf57a5c99c88f06710489a26f3
chanmic <michaelchan1380@gmail.com>
2021-06-01 07:34:35 -0700

    Update submission.service.ts

--- src/app/services/submission/submission.service.ts
+++ src/app/services/submission/submission.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ISubmission } from 'src/app/models/isubmission-submission';
 import { environment } from 'src/environments/environment';
@@ -8,10 +8,17 @@ import { environment } from 'src/environments/environment';
 })
 export class SubmissionService {
   private endpoint: string = environment.apiUrl+'/submissions'
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '+localStorage.getItem('token')
+    }),
+    withCredentials: true
+  };
 
-  constructor(private http:  HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   submit(body: ISubmission) {
-    return this.http.put(this.endpoint+'/', body).pipe();
+    return this.http.put(this.endpoint+'/', body, this.httpOptions).pipe();
   }
 }
