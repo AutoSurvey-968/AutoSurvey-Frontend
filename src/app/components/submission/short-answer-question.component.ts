cb27a03f7c3fc4262cc931f98f2d81322b525236
chanmic <michaelchan1380@gmail.com>
2021-05-31 17:59:36 -0700

    Another typo.

--- src/app/components/submission/short-answer-question.component.ts
+++ src/app/components/submission/short-answer-question.component.ts
@@ -6,8 +6,8 @@ import { IQuestion } from 'src/app/models/iquestion.question';
   template: `
     <br><hr><br>
     <div class="mb-3">
-      <label for="{{'question-'+index+'-'+'choice'+'i'}}" class="form-label">{{ question.title }}<span style="color:red" *ngIf="question.isRequired">*</span></label>
-      <textarea class="form-control" id="{{'question-'+index+'-'+'choice'+'i'}}" rows="3"></textarea>
+      <label for="{{'question-'+index+'-'+'form'}}" class="form-label">{{ question.title }}<span style="color:red" *ngIf="question.isRequired">*</span></label>
+      <textarea class="form-control" id="{{'question-'+index+'-'+'form'}}" rows="3"></textarea>
     </div>
   `,
   styleUrls: ['./submission.component.css']
