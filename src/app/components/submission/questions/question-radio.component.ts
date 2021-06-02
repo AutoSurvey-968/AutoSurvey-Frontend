8944771f8a8404136b830339a6aab0445375dc73
chanmic <michaelchan1380@gmail.com>
2021-06-02 12:58:54 -0700

    Radio component change.

--- src/app/components/submission/questions/question-radio.component.ts
+++ src/app/components/submission/questions/question-radio.component.ts
@@ -11,15 +11,34 @@ import { IQuestion } from 'src/app/models/iquestion-question';
       <div class="d-flex justify-content-center" formArrayName="responses">
         <!-- Need to see how we grab these labels from questions -->
         <p style="margin-top: 9px; margin-right: 10px;">Not Satisfied</p>
-        <div class="question-group form-check form-check-inline" [formGroupName]="index" *ngFor="let choice of question.choices; index as i">
+        <div class="question-group form-check form-check-inline" [formGroupName]="index">
           <!--id is formatted as question-index-choice-i, example: question-0-choice-0
               There also needs to be a name, but it needs to match formControlName, so
               it gets messy when there are multiple radio questions. For now, you can
               select multiple radio buttons, but only the most recent will be stored in
-              the form on submit. The next person will need to figure this out.-->
-          <input type="radio" formControlName="response" id="{{'question-'+index+'-'+'choice-'+i}}" value="{{choice}}">
-          <label for="{{'question-'+index+'-'+'choice-'+i}}">{{choice}}</label>
+              the form on submit. The next person will need to figure this out.
+              Because of the Survey service side of the front-end, these options are no
+              longer populated by choices. This is hard coded 1 through 5.-->
+          <input type="radio" formControlName="response" id="{{'question-'+index+'-'+'choice-1'}}" value="1">
+          <label for="{{'question-'+index+'-'+'choice-1'}}">1</label>
         </div>
+        <div class="question-group form-check form-check-inline" [formGroupName]="index">
+          <input type="radio" formControlName="response" id="{{'question-'+index+'-'+'choice-2'}}" value="2">
+          <label for="{{'question-'+index+'-'+'choice-2'}}">2</label>
+        </div>
+        <div class="question-group form-check form-check-inline" [formGroupName]="index">
+          <input type="radio" formControlName="response" id="{{'question-'+index+'-'+'choice-3'}}" value="3">
+          <label for="{{'question-'+index+'-'+'choice-3'}}">3</label>
+        </div>
+        <div class="question-group form-check form-check-inline" [formGroupName]="index">
+          <input type="radio" formControlName="response" id="{{'question-'+index+'-'+'choice-4'}}" value="4">
+          <label for="{{'question-'+index+'-'+'choice-4'}}">4</label>
+        </div>
+        <div class="question-group form-check form-check-inline" [formGroupName]="index">
+          <input type="radio" formControlName="response" id="{{'question-'+index+'-'+'choice-5'}}" value="5">
+          <label for="{{'question-'+index+'-'+'choice-5'}}">5</label>
+        </div>
+
 
         <p style="margin-top: 9px; margin-left: 10px;">Very Satisfied</p>
       </div>
