96a3d5280984139751debc615d8fc602ffd01130
chanmic <michaelchan1380@gmail.com>
2021-06-01 21:50:07 -0700

    Submission service front end complete.

--- src/app/components/submission/questions/question-checkbox.component.ts
+++ src/app/components/submission/questions/question-checkbox.component.ts
@@ -10,7 +10,10 @@ import { IQuestion } from 'src/app/models/iquestion-question';
       <p>{{ question.title }}<span *ngIf="question.isRequired">*</span></p>
       <div formArrayName="responses">
         <div class="form-check" [formGroupName]="index" *ngFor="let choice of question.choices; index as i">
-          <!--id is formatted as question-index-choice-i, example: question-0-choice-0-->
+          <!--id is formatted as question-index-choice-i, example: question-0-choice-0
+              Checkbox doesn't work properly. When submitting the form, the value is
+              a boolean rather than an array of values. The next person will need to
+              figure this out.-->
           <input type="checkbox" formControlName="response" id="{{'question-'+index+'-'+'choice-'+i}}" value="{{choice}}">
           <label class="form-check-label" for="{{'question-'+index+'-'+'choice-'+i}}">{{choice}}</label>
         </div>
--- src/app/components/submission/questions/question-radio.component.ts
+++ src/app/components/submission/questions/question-radio.component.ts
@@ -12,8 +12,12 @@ import { IQuestion } from 'src/app/models/iquestion-question';
         <!-- Need to see how we grab these labels from questions -->
         <p style="margin-top: 9px; margin-right: 10px;">Not Satisfied</p>
         <div class="question-group form-check form-check-inline" [formGroupName]="index" *ngFor="let choice of question.choices; index as i">
-          <!--id is formatted as question-index-choice-i, example: question-0-choice-0-->
-          <input type="radio" formControlName="response" name="response" id="{{'question-'+index+'-'+'choice-'+i}}" value="{{choice}}">
+          <!--id is formatted as question-index-choice-i, example: question-0-choice-0
+              There also needs to be a name, but it needs to match formControlName, so
+              it gets messy when there are multiple radio questions. For now, you can
+              select multiple radio buttons, but only the most recent will be stored in
+              the form on submit. The next person will need to figure this out.-->
+          <input type="radio" formControlName="response" id="{{'question-'+index+'-'+'choice-'+i}}" value="{{choice}}">
           <label for="{{'question-'+index+'-'+'choice-'+i}}">{{choice}}</label>
         </div>
 
