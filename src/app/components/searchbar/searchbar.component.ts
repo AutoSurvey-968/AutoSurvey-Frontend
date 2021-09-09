856f95ea06c76b3ebe1fd097e15df819ab353713
khinethet <khinethet015@gmail.com>
2021-09-09 14:56:34 -0400

    searchbar

--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { SurveyService } from 'src/app/services/survey/survey.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.css']
+  styleUrls: ['./searchbar.component.css'],
 })
 export class SearchbarComponent implements OnInit {
+  public searchInput: String = '';
+  public searchResult: String = '';
 
-  constructor() { }
+  constructor(
+    public surveyService: SurveyService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
+    // this.surveyService
+    //   .getSurveyByTitle(this.searchInput)
+    //   .subscribe((data) => this.searchResult);
   }
 
+  search(): void {
+    if (this.searchInput)
+      this.router.navigateByUrl('/search/' + this.searchInput);
+  }
 }
