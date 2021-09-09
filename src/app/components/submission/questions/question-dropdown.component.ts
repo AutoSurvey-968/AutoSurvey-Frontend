5efbb8fbacc4e7ca39121e809f1da10bca17cfca
mcinerneym <mcinerney.michael.t@gmail.com>
2021-09-09 08:10:50 -0700

    Fixed the data issues around submissions

--- src/app/components/submission/questions/question-dropdown.component.ts
+++ src/app/components/submission/questions/question-dropdown.component.ts
@@ -10,7 +10,7 @@ import { IQuestion } from 'src/app/models/iquestion-question';
       <div formArrayName="responses">
         <div [formGroupName]="index">
           <label class="form-label" for="{{'question-'+index+'-dropdown'}">{{question.title}}<span *ngIf="question.isRequired">*</span></label>
-          <select class="form-control" id="{{'question-'+index+'-dropdown'}" formControlName="response" required>
+          <select class="form-control" id="{{'question-'+index+'-dropdown'}" formControlName="response{{index}}" required>
             <!--id is formatted as question-index-choice-i, example: question-0-choice-0-->
             <option *ngFor="let choice of question.choices; index as i" id="{{'question-'+index+'-'+'choice-'+i}}"  value="{{choice}}">{{choice}}</option>
           </select>
